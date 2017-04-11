a01cdf1a9151c2dbfad94be4028cb309d6d99169
JamesONeill-III <joneill313@Hotmail.com>
2017-04-10 21:32:15 -0700

    Add files via upload

---
+++ Basic_StepMotor_Sketch.ino
@@ -0,0 +1,22 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 48;
+
+Stepper myStepper(stepsPerRevolution, 8,9,10,11);
+void setup() {
+
+  myStepper.setSpeed(300);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+Serial.println("clockwise");
+myStepper.step(stepsPerRevolution);
+delay(500);
+
+Serial.println("counterclockwise");
+myStepper.step(-stepsPerRevolution);
+delay(500);
+}
